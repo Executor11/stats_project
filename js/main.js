09543ac4766eed8fa8dad7e61458e30b1f66da03
Leonid Zakhariants <leonid110400@gmail.com>
2021-07-10 02:44:56 +0400

    added : heroes folder and images now work second page - statistic page added heroes and info about win rate, played games and other information now we can sort by Sorting mode (win rate and played games) other selections sorting randomly

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
+//randomizer
+const randomNum = (min, max) => ~~(Math.random() * (max - min + 1) + min);
+const arrayRandomizer = array => [...array].sort(() => 0.5 - Math.random());
+
+//click
 $(".my_account").on("click", () => {
+  $(".nav_list .active").removeClass("active");
+  $(".my_account").addClass("active");
   $(".content_renderer").hide().show()
     .html(`  <section class="header">My account</section>
     <section class="player info">
@@ -57,7 +64,7 @@ $(".my_account").on("click", () => {
           <img src="img/anna.png" alt="image" />
         </div>
         <div class="stats">
-          <div class="win_rate">Win rate <strong>81.3%</strong></div>
+          <div class="win_rate">Win rate <strong>61.31%</strong></div>
           <div class="rate font-18">Anna</div>
           <div class="count">
             <i class="far fa-plus-square font-20 blue bold"></i>
@@ -276,10 +283,6 @@ $(".my_account").on("click", () => {
       </div>
     </section>`);
 
-  //randomizer
-  const randomNum = (min, max) => ~~(Math.random() * (max - min + 1) + min);
-  const arrayRandomizer = array => [...array].sort(() => 0.5 - Math.random());
-
   // progress bar
 
   let lockedRefresher = true;
@@ -418,12 +421,16 @@ $(".my_account").on("click", () => {
   });
 });
 $(".my_statistics").on("click", () => {
+  //active
+  $(".nav_list .active").removeClass("active");
+  $(".my_statistics a").addClass("active");
+  //render
   $(".content_renderer").html(`
   <section class="header">Statistic</section>
   <section class="search_hero">
         <div class="path">
           <span class="silver font-12">Path</span>
-          <select name="path" class="search_hero_select">
+          <select name="path" class="search_hero_select random">
             <option value="2.52.0.8">2.52.0.8</option>
             <option value="2.52.0.6">2.52.0.6</option>
             <option value="2.52.0.2">2.52.0.2</option>
@@ -432,7 +439,7 @@ $(".my_statistics").on("click", () => {
         </div>
       <div class="role">
         <span class="silver font-12">Role</span>
-        <select name="path" class="search_hero_select">
+        <select name="path" class="search_hero_select random">
           <option value="All">All</option>
           <option value="Healer">Healer</option>
           <option value="Slayer">Slayer</option>
@@ -441,7 +448,7 @@ $(".my_statistics").on("click", () => {
       </div>
       <div class="game_mode">
         <span class="silver font-12">Game mode</span>
-        <select name="path" class="search_hero_select">
+        <select name="path" class="search_hero_select random">
           <option value="Rating">Rating</option>
           <option value="Solo">Solo</option>
           <option value="Friendly">Friendly</option>
@@ -450,7 +457,7 @@ $(".my_statistics").on("click", () => {
       </div>
       <div class="map">
         <span class="silver font-12">Map</span>
-        <select name="path" class="search_hero_select">
+        <select name="path" class="search_hero_select random">
           <option value="Temple">Temple</option>
           <option value="Road">Road</option>
           <option value="Walley">Walley</option>
@@ -459,7 +466,7 @@ $(".my_statistics").on("click", () => {
       </div>
       <div class="hero_rank">
         <span class="silver font-12">Hero rank</span>
-        <select name="path" class="search_hero_select">
+        <select name="path" class="search_hero_select random">
           <option value="Silver">Silver</option>
           <option value="Gold">Gold</option>
           <option value="Diamond">Diamond</option>
@@ -467,15 +474,15 @@ $(".my_statistics").on("click", () => {
         </select>
       </div>
       <div class="graphic_mode">
-        <span class="silver font-12">Graphic mode</span>
-        <select name="path" class="search_hero_select">
+        <span class="silver font-12">Sorting mode</span>
+        <select name="filter_mode" class="search_hero_select filter_mode">
           <option value="winRate">Win Rate</option>
           <option value="playedGames">Played Games</option>
         </select>
       </div>
       <div class="hero">
         <span class="silver font-12">Hero</span>
-        <select name="path" class="search_hero_select">
+        <select name="path" class="search_hero_select random">
           <option value="All">All</option>
           <option value="Healer">Healer</option>
           <option value="Slayer">Slayer</option>
@@ -487,7 +494,7 @@ $(".my_statistics").on("click", () => {
       </div>
   </section>
   <section class="search_result">
-    <div class="header">
+    <section class="header">
         <div class="hero">
           <div class="hero_name">
             <span class="silver font-14">Hero</span>
@@ -514,52 +521,139 @@ $(".my_statistics").on("click", () => {
         <div class="talents">
           <span class="silver font-14">Talents</span>
           </div>
-    </div>
+    </section>
     
   
   </section>
   `);
-const heroResult = [
-{
-  img: '../img/irel.jpg',
-  name: 'Irel',
-  winRate: 51.02,
-  changes: -2.01,
-  assurance: '+3.17',
-  selection: 4.66,
-  banned: 1.37,
-  playedGames: 967
-},
-{
-  img: '../img/irel.jpg',
-  name: 'Irel',
-  winRate: 51.02,
-  changes: -2.01,
-  assurance: '+3.17',
-  selection: 4.66,
-  banned: 1.37,
-  playedGames: 1234,
-},
+  //hero result section for find heroes
+  const heroResult = [
+    {
+      img: "../img/heroes/Ana_square_tile.png",
+      name: "Anna",
+      winRate: 61.62,
+      changes: "+1.22",
+      assurance: "+7.17",
+      selection: 9.81,
+      banned: 3.11,
+      playedGames: 981,
+    },
+    {
+      img: "../img/irel.jpg",
+      name: "Yrel",
+      winRate: 59.22,
+      changes: "-2.01",
+      assurance: "+3.17",
+      selection: 4.66,
+      banned: 1.37,
+      playedGames: 712,
+    },
+    {
+      img: "../img/heroes/sylvana.jpg",
+      name: "Sylvana",
+      winRate: 57.95,
+      changes: "-1.71",
+      assurance: "+9.55",
+      selection: 4.22,
+      banned: 6.59,
+      playedGames: 939,
+    },
+    {
+      img: "../img/heroes/thrall.jpg",
+      name: "Thrall",
+      winRate: 56.72,
+      changes: "+5.61",
+      assurance: "-3.97",
+      selection: 4.66,
+      banned: 1.37,
+      playedGames: 967,
+    },
+    {
+      img: "../img/heroes/Uther_Hero_Portrait.png",
+      name: "Uther",
+      winRate: 55.02,
+      changes: "+3.28",
+      assurance: "+1.33",
+      selection: 8.91,
+      banned: 3.87,
+      playedGames: 669,
+    },
+    {
+      img: "../img/heroes/reksar.jpg",
+      name: "Rexxar",
+      winRate: 53.55,
+      changes: "+1.11",
+      assurance: "-5.66",
+      selection: 7.99,
+      banned: 8.57,
+      playedGames: 481,
+    },
+    {
+      img: "../img/heroes/cho.jpg",
+      name: "Cho",
+      winRate: 51.02,
+      changes: "-2.21",
+      assurance: "-1.11",
+      selection: 9.16,
+      banned: 4.17,
+      playedGames: 557,
+    },
+    {
+      img: "../img/heroes/gall.jpg",
+      name: "Gall",
+      winRate: 50.51,
+      changes: "+5.31",
+      assurance: "+1.17",
+      selection: 6.11,
+      banned: 3.26,
+      playedGames: 711,
+    },
+    {
+      img: "../img/heroes/Anduin_square_tile.png",
+      name: "Anduin",
+      winRate: 50.22,
+      changes: "+3.01",
+      assurance: "-1.17",
+      selection: 6.16,
+      banned: 2.37,
+      playedGames: 717,
+    },
 
-]
+    {
+      img: "../img/heroes/Jaina_square_tile.png",
+      name: "Jaina",
+      winRate: 49.26,
+      changes: "+8.01",
+      assurance: "-1.17",
+      selection: 1.66,
+      banned: 6.37,
+      playedGames: 517,
+    },
+  ];
 
-
-heroResult.forEach((e,i)=>{
-  $('.search_result').append(`<div class="hero_played">
-  <div class="hero">
-    <div class="hero_name">
-      <div  class="hero_avatar" ></div>
-      <span class="black font-18">${e.name}</span>
-    </div>
-  </div>
+  heroResult.forEach((e, i) => {
+    $(".search_result").append(`
+    <div class="hero_played">
+      <div class="hero">
+        <div class="hero_name">
+          <div  class="hero_avatar" style="background-image: url(${
+            e.img
+          })"></div>
+            <span class="black font-18">${e.name}</span>
+          </div>
+        </div>
   <div class="mode">
     <span class="black font-14">${e.winRate}</span>
   </div>
   <div class="map">
-    <span class=" font-14 red">${e.changes}</span>
+    <span class=" font-14 ${+e.changes >= 0 ? "green" : "red"}">${
+      e.changes
+    }</span>
   </div>
   <div class="rating">
-    <span class="green font-14">${e.assurance}</span>
+    <span class=" font-14 ${+e.assurance >= 0 ? "green" : "red"}">${
+      e.assurance
+    }</span>
   </div>
   <div class="result">
     <span class="black font-14">${e.selection}</span>
@@ -567,13 +661,42 @@ heroResult.forEach((e,i)=>{
   <div class="time">
     <span class="black font-14">${e.banned}</span>
   </div>
-  <div class="time">
+  <div class="time playedGames">
     <span class="black font-14">${e.playedGames}</span>
   </div>
   <div class="talents">
     <span class=" font-14 show_btn">Show</span>
     </div>
 </div>`);
-})
+  });
 
+  $(".filter_mode").on("change", e => {
+    if (e.target.value == "winRate") {
+      //if win rate sort
+      $(".search_result")
+        .children("div")
+        .sort(
+          (a, b) =>
+            +$(b).children(".mode").text().trim() -
+            +$(a).children(".mode").text().trim()
+        )
+        .appendTo(".search_result");
+    } else {
+      //if played games sort
+      $(".search_result")
+        .children("div")
+        .sort(
+          (a, b) =>
+            +$(b).children(".playedGames").text().trim() -
+            +$(a).children(".playedGames").text().trim()
+        )
+        .appendTo(".search_result");
+    }
+  });
+  $(".random").on("change", e => {
+    $(".search_result")
+      .children("div")
+      .sort(() => 0.5 - Math.random())
+      .appendTo(".search_result");
+  });
 });
